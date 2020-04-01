cda91f6a0d7ef5841ac458804266dd5ddf951c18
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-01 00:36:30 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,5 +5,8 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
+  array.count do |i| 
+    i == i.to_s && i.length == 0 
+  end
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
